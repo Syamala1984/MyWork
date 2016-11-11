e16aa42c0c459e1b2ebb10ab3fdfe2cd7caf73f5
Syamala Manam <manam.syamala@gmail.com>
2016-11-11 09:31:26 -0800

    Added OnlineNotesApplication DbScript and application

---
+++ OnlineNotesProject/DBScript/notepad.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE `notepaddb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `folder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `folder_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder_id_idx` (`folder_id`),
+  CONSTRAINT `folder_id` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `ssn` varchar(45) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ssn_UNIQUE` (`ssn`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
